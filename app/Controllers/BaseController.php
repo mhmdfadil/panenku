ee8a3ce711a34403f12c935969c86c6ded5e2c28
mhmdfadil <muhammad.210170034@mhs.unimal.ac.id>
2026-07-06 18:27:04 +0700

    fix error userId basecontroller

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,39 +7,65 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
-/**
- * BaseController provides a convenient place for loading components
- * and performing functions that are needed by all your controllers.
- *
- * Extend this class in any new controllers:
- * ```
- *     class Home extends BaseController
- * ```
- *
- * For security, be sure to declare any new methods as protected or private.
- */
 abstract class BaseController extends Controller
 {
+    
     /**
-     * Be sure to declare properties for any property fetch you initialized.
-     * The creation of dynamic property is deprecated in PHP 8.2.
+     * Preload helper.
+     *
+     * @var array
      */
-
-    // protected $session;
+    protected $helpers = ['url', 'form', 'text', 'html'];
 
     /**
-     * @return void
+     * Constructor.
      */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
-    {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
+    public function initController(
+        RequestInterface $request,
+        ResponseInterface $response,
+        LoggerInterface $logger
+    ): void {
         parent::initController($request, $response, $logger);
+    }
 
-        // Preload any models, libraries, etc, here.
-        // $this->session = service('session');
+    /**
+     * Ambil ID user dari session.
+     */
+    protected function userId(): int
+    {
+        return (int) session()->get('user_id');
+    }
+
+    /**
+     * Response JSON umum.
+     */
+    protected function jsonResponse(array $data, int $status = 200): ResponseInterface
+    {
+        return $this->response
+            ->setStatusCode($status)
+            ->setJSON($data);
+    }
+
+    /**
+     * Response sukses.
+     */
+    protected function successJson(string $message, array $data = []): ResponseInterface
+    {
+        return $this->jsonResponse([
+            'status'  => 'success',
+            'message' => $message,
+            'data'    => $data,
+        ]);
+    }
+
+    /**
+     * Response error.
+     */
+    protected function errorJson(string $message, int $status = 400): ResponseInterface
+    {
+        return $this->jsonResponse([
+            'status'  => 'error',
+            'message' => $message,
+        ], $status);
     }
 }
