ad5f8ce6d08712e9aa5e155f9d1853e670fa94d2
mhmdfadil <muhammad.210170034@mhs.unimal.ac.id>
2026-07-08 04:41:42 +0700

    penyesuaian dashboard statistik data

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,13 +2,33 @@
 
 namespace App\Controllers;
 
+use App\Models\PanenModel;
+use App\Models\LahanModel;
+use App\Models\TanamanModel;
+
 class Dashboard extends BaseController
 {
     public function index()
     {
+        $panenModel   = new PanenModel();
+        $lahanModel   = new LahanModel();
+        $userId       = $this->userId();
+
+        $stats        = $panenModel->getDashboardStats($userId);
+        $totalLahan   = $lahanModel->getTotalLuas($userId);
+        $recentPanen  = $panenModel->getRecentPanen($userId);
+        $upcoming     = $panenModel->getUpcomingPanen($userId);
+        $grafik6Bulan = $panenModel->getProduksi6Bulan($userId);
+        $perKomoditas = $panenModel->getProduksiPerKomoditas($userId);
 
         return view('dashboard/index', [
             'title'         => 'Dashboard',
+            'stats'         => $stats,
+            'totalLahan'    => $totalLahan,
+            'recentPanen'   => $recentPanen,
+            'upcoming'      => $upcoming,
+            'grafik6Bulan'  => $grafik6Bulan,
+            'perKomoditas'  => $perKomoditas,
         ]);
     }
 }
